61a6ccb4cf8d4e0e2e0f20ea8259a8d9cf5e352c
epender <epender@umich.edu>
2020-05-15 12:04:35 -0400

    Make nucleus use the joint system

--- Assets/Scripts/CellParts/Nucleus.cs
+++ Assets/Scripts/CellParts/Nucleus.cs
@@ -1,84 +1,41 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Nucleus : SimplePart {
     public float accel = 1;
-    public List<SpringJoint2D> stickyJoints;
+    public List<JointWrapper> stickyJoints;
     public bool isSticky = true;
 
     public override void Awake() {
         base.Awake();
         if (stickyJoints == null) {
-            stickyJoints = new List<SpringJoint2D>();
+            stickyJoints = new List<JointWrapper>();
         }
     }
 
     public override void OnCellPartEnterNearby(SimplePart cp) {
         JointWrapper.MakeJoint(this, cp);
     }
-
-    void OnCollisionEnter2D(Collision2D collision) {
-        if (isSticky) {
-            foreach (var joint in stickyJoints) {
-                if (joint.connectedBody == collision.rigidbody) {
-                    return;
-                }
-            }
-            SpringJoint2D sticky = gameObject.AddComponent<SpringJoint2D>();
-            sticky.connectedBody = collision.rigidbody;
-            sticky.distance = .5f;
-            sticky.frequency = 2;
-            sticky.enableCollision = true;
-            stickyJoints.Add(sticky);
-        }
-    }
-    
-    //public override void ConfigureJointConstants(JointWrapper wrap) {
-    //    var joint = wrap.GetOrMakeJoint<DistanceJoint2D>();
-    //    
-    //    joint.enableCollision = true;
-    //    joint.autoConfigureConnectedAnchor = false;
-    //    joint.autoConfigureDistance = false;
-    //    joint.maxDistanceOnly = true;
-    //    joint.distance = Distance(wrap.GetConnected());
-    //    joint.breakForce = 20;
-    //}
     public override void ConfigureJointConstants(JointWrapper wrap) {
-        var joint = wrap.GetOrMakeJoint<DistanceJoint2D>();
-        
-        joint.enableCollision = true;
-        joint.autoConfigureConnectedAnchor = false;
-        joint.autoConfigureDistance = false;
-        joint.maxDistanceOnly = true;
-        joint.distance = Distance(wrap.GetConnected());
-        joint.breakForce = 20;
+        SpringJoint2D sticky = wrap.GetOrMakeJoint<SpringJoint2D>();
+        sticky.distance = .5f;
+        sticky.frequency = 2;
+        sticky.enableCollision = true;
+        stickyJoints.Add(wrap);
     }
 
     public override void Update() {
         if (!isSticky && stickyJoints.Count > 0) {
             foreach (var joint in stickyJoints) {
-                Destroy(joint);
+                joint.Destroy();
             }
             stickyJoints.Clear();
         }
         base.Update();
-
-        //foreach (SimplePart part in gameObject.GetComponentInChildren<NearbyDetector>().nearby) {
-        //    Vector2 dir = (part.transform.position - transform.position);
-        //    if (dir.magnitude < 1) {
-        //        JointWrapper.MakeJoint(this, part);
-        //    } else {
-        //        dir.Normalize();
-        //        part.body.AddForce(-dir*force);
-        //        body.AddForce(dir*force);
-        //    }
-        //}
-        ////TODO :apply a force to every nearby cell
     }
 
     public override float GetNearbyRadius() {
-        return GetRadius() + 1;
+        return GetRadius() + .1f;
     }
 
     public override float JointDesire(SimplePart sp) { 
